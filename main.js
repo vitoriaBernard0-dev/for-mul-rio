56273b94cad1ae6b4efb43e9d3a895b7261f0e3b
vitoriaBernard0-dev <vi.bernardo12@gmail.com>
2023-03-17 15:13:50 -0300

    javascript

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ var nomeUsuario = "Astolfo";
 var senha = "2023";
 // função
 function verify() {
-    if (nomeUsuario == "Astolfo" && senha == "2023") {
+    var tentativa1 = document.getElementById("usuario").value;
+    var tentativa2 = document.getElementById("password").value;
+
+    if (nomeUsuario == tentativa1 && senha == tentativa2) {
         alert("Senha correta, entrando no sistema...");
         window.location.href = "https://www.sp.senai.br/"
     } else {
